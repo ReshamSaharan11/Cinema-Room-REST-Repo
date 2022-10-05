08b677de5541da655d27450adcd612a1372c1f18
Resham_29 <rishabsaharan11@gmail.com>
2022-10-05 15:14:54 +0530

    CINEMA REST SERVICE

---
+++ Cinema Room REST Service/Topics/Package/Getting input/src/Main.java
@@ -0,0 +1,13 @@
+
+
+class InputReader {
+    public static String getString() {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        String s = scanner.nextLine();
+
+        return s;
+
+    }
+}
